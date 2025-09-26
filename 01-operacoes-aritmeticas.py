764cfa08dfc4c236587044d2665e717b278fa7a3
Fábio Milagres <fabioemanuellmilagres@gmail.com>
2025-09-26 10:16:25 -0300

    adicionado operações artiméticas básicas

---
+++ 01-operacoes-aritmeticas.py
@@ -0,0 +1,18 @@
+a = 10
+b = 3
+
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+divisao_inteira = a // b
+resto = a % b
+exponenciacao = a**b
+
+print(f"Soma: {soma}")
+print(f"Subtração: {subtracao}")
+print(f"Multiplicação: {multiplicacao}")
+print(f"Divisão: {divisao}")
+print(f"Divisão Inteira: {divisao_inteira}")
+print(f"Resto: {resto}")
+print(f"Exponenciação: {exponenciacao}")
